efd37829fa0c79d52721094c15016a2a6fef6280
mbettez <michaela.bettez@gmail.com>
2019-04-14 10:25:26 -0400

    FSR code update

--- FSRsketchcode/FSRsketchcode.ino
+++ FSRsketchcode/FSRsketchcode.ino
@@ -1,19 +1,6 @@
+
+
 #include <math.h>       /* pow */
-
-int Vout1 = A3;
-int Vout2 = A4;
-int Vout3 = A5;
-float Vin = 1024; // 3.3V in units of V, assumed to the same for all of the FSRs
-float R1 = 5100; // in units of Ohms, same for all FSRs
-float FSR_1 = 0;
-float FSR_2 = 0;
-float FSR_3 = 0;
-float a = 9622;
-float b = -1.357;
-int force1 = 0;
-int force2 = 0;
-int force3 = 0;
-
 #include <Arduino.h>
 #include <SPI.h>
 #include "Adafruit_BLE.h"
@@ -81,6 +68,20 @@ Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_
 //                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
 //                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
+
+int Vout1 = A3;
+int Vout2 = A4;
+int Vout3 = A5;
+float Vin = 1024; // 3.3V in units of V, assumed to the same for all of the FSRs
+float R1 = 5100; // in units of Ohms, same for all FSRs
+float FSR_1 = 0;
+float FSR_2 = 0;
+float FSR_3 = 0;
+float a = 9622;
+float b = -1.357;
+int force1 = 0;
+int force2 = 0;
+int force3 = 0;
 
 // A small helper
 void error(const __FlashStringHelper*err) {
@@ -150,18 +151,18 @@ void setup() {
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
-// For FSR 1
-FSR_1 = (( R1 * Vout1 ) / Vin ) / ( 1 - ( Vout1 / Vin ));
-force1 = int(pow(a * FSR_1, b));
-// For FSR 2
-FSR_2 = (( R1 * Vout2 ) / Vin ) / ( 1 - ( Vout2 / Vin ));
-force2 = int(pow(a * FSR_2, b));
-// For FSR 3
-FSR_3 = (( R1 * Vout3 ) / Vin ) / ( 1 - ( Vout3 / Vin ));
-force3 = int(pow(a * FSR_3, b));
-
-// from bleuart loop
+    // put your main code here, to run repeatedly:
+    // For FSR 1
+    FSR_1 = (( R1 * Vout1 ) / Vin ) / ( 1 - ( Vout1 / Vin ));
+    force1 = int(pow(a * FSR_1, b));
+    // For FSR 2
+    FSR_2 = (( R1 * Vout2 ) / Vin ) / ( 1 - ( Vout2 / Vin ));
+    force2 = int(pow(a * FSR_2, b));
+    // For FSR 3
+    FSR_3 = (( R1 * Vout3 ) / Vin ) / ( 1 - ( Vout3 / Vin ));
+    force3 = int(pow(a * FSR_3, b));
+    
+    // from bleuart loop
     //n = Serial.readBytes(inputs, BUFSIZE);
     //inputs[n] = 0;
     // Send characters to Bluefruit
@@ -176,7 +177,7 @@ force3 = int(pow(a * FSR_3, b));
     ble.print(force1);
     ble.print(force2);
     ble.print(force3);
-  }
+  
 
   // Echo received data
   while ( ble.available() )
@@ -190,4 +191,5 @@ force3 = int(pow(a * FSR_3, b));
     if (c <= 0xF) Serial.print(F("0"));
     Serial.print(c, HEX);
     Serial.print("] ");
+  }
 }
